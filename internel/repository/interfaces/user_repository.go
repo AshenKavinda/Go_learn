aef93903a99add71334f93e990a4250aa62ba71e
AshenKavinda <kavindahemarathna321@gmail.com>
2026-07-15 21:04:21 +0530

    user repository

--- internel/repository/interfaces/user_repository.go
+++ internel/repository/interfaces/user_repository.go
@@ -7,5 +7,9 @@ import (
 )
 
 type UserRepository interface {
-	Create(ctx context.Context, user *models.User) error
+	Create(ctx context.Context, user *models.User) (*int64, error)
+	GetAll(ctx context.Context) (*[]models.User, error)
+	GetByID(ctx context.Context, id int64) (*models.User, error)
+	Update(ctx context.Context, user *models.User) (*int, error)
+	Delete(ctx context.Context, id int64) (*int, error)
 }
--- internel/repository/user_repository.go
+++ internel/repository/user_repository.go
@@ -16,6 +16,49 @@ func NewUserRepository(db *gorm.DB) repositoryInterfaces.UserRepository {
 	return &UserRepository{DB: db}
 }
 
-func (s *UserRepository) Create(ctx context.Context, user *models.User) error {
-	return nil
+func (s *UserRepository) Create(ctx context.Context, user *models.User) (*int64, error) {
+	err := gorm.G[models.User](s.DB).Create(ctx, user)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user.ID, nil
+}
+
+func (s *UserRepository) GetAll(ctx context.Context) (*[]models.User, error) {
+	posts, err := gorm.G[models.User](s.DB).Find(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &posts, nil
+}
+
+func (s *UserRepository) GetByID(ctx context.Context, id int64) (*models.User, error) {
+	user, err := gorm.G[models.User](s.DB).Where("id = ?", id).First(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (s *UserRepository) Update(ctx context.Context, user *models.User) (*int, error) {
+	ra, err := gorm.G[models.User](s.DB).Where("id = ?", user.ID).Updates(ctx, *user)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ra, nil
+}
+
+func (s *UserRepository) Delete(ctx context.Context, id int64) (*int, error) {
+	ra, err := gorm.G[models.User](s.DB).Where("id = ?", id).Delete(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ra, nil
+
 }
